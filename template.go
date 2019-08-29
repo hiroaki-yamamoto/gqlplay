2816b016d13643088b8036dbc29751ea0684ceab
Hiroaki Yamamoto <hiroaki@hysoftware.net>
2019-08-29 20:18:01 +0900

    Removed /

--- template.go
+++ template.go
@@ -8,8 +8,8 @@ var gqltmp = `
   <meta name="viewport" content="user-scalable=no, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, minimal-ui">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>GraphQL Playground</title>
-  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/graphql-playground-react/build/static/css/index.css" />
-  <link rel="shortcut icon" href="//cdn.jsdelivr.net/npm/graphql-playground-react/build/favicon.png" />
+  <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/graphql-playground-react/build/static/css/index.css">
+  <link rel="shortcut icon" href="//cdn.jsdelivr.net/npm/graphql-playground-react/build/favicon.png">
   <script src="//cdn.jsdelivr.net/npm/graphql-playground-react/build/static/js/middleware.js"></script>
   <style type="text/css">
     html {
